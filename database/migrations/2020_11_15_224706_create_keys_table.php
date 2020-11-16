0514eb77b221e557831d74564dbc5a02beae16d0
Igor <igortochihara@gmail.com>
2020-11-15 21:19:47 -0300

    Fix migration application

---
+++ database/migrations/2020_11_15_224706_create_keys_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKeysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keys', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('hash');
+            $table->string('name');
+            $table->enum('availability', ['unavailable', 'available'])->default('unavailable');
+            $table->foreign('room_id')->references('id')->on('room')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keys');
+    }
+}
---
+++ database/migrations/2020_11_15_232640_create_borrowings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBorrowingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('borrowings', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('hash');
+            $table->dateTime('borrowing_time');
+            $table->dateTime('return_time')->nullable();
+            $table->foreign('key_id')->references('id')->on('keys')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('borrowings');
+    }
+}
